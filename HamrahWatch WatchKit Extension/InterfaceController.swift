40088eed11df18c5bcf32d1e9cab9d66cff719e0
rezakashkoul <rezakashkoul@gmail.com>
2021-12-12 12:39:21 +0330

    Initial Commit

---
+++ HamrahWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  HamrahWatch WatchKit Extension
+//
+//  Created by Reza Kashkoul on 21/Azar/1400 .
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ HamrahWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HamrahWatch
+//
+//  Created by Reza Kashkoul on 21/Azar/1400 .
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
